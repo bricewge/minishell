46738f8df85926c52e7d5cadc36700d221502e9e
Brice Waegeneire <brice.wge@gmail.com>
2017-10-02 13:26:35 +0200

    ft_memalloc inverse logic

--- libft/src/malloc/ft_memalloc.c
+++ libft/src/malloc/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/24 17:34:04 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/26 11:00:59 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/10/02 12:58:43 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	*ft_memalloc(size_t size)
 {
 	void	*result;
 
-	result = (void *)malloc(sizeof(*result) * size);
-	if (result)
-	{
-		ft_memset(result, 0, size);
-		return (result);
-	}
-	return (NULL);
+	result = (void *)malloc(size);
+	if (!result)
+		return (NULL);
+	ft_memset(result, 0, size);
+	return (result);
 }
